f27fc1080cd3274fd203008b98dbbb85b38b4280
ofir-frd <ofir.friedman@materials.zone>
2022-07-30 14:53:12 +0300

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
-import PropTypes from "prop-types"
+import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({ title }) => {
+    const onClick = () =>{
+        console.log('Add clicked')
+    }
+
     return (
-        <header>
-            <h1>{title}</h1>
+        <header className={'header'}>
+            <h1 style={{ }}>{title}</h1>
+            <Button color='green' text='Add' onClick={onClick}/>
         </header>
     )
 }
