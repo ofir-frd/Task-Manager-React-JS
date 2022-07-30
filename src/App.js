1d6eb99a06feb3829a8ebe0395a162e1c73f1138
ofir-frd <ofir.friedman@materials.zone>
2022-07-30 15:21:07 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import {useState} from 'react'
+
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
 function App() {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Buy Milk',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: 'Walk the dog',
+            day: 'Feb 6th at 6:20pm',
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: 'Meeting',
+            day: 'Feb 7th at 13:00pm',
+            reminder: true,
+        }
+    ])
+
   return (
     <div className="container">
       <Header />
-      <Tasks/>
+      <Tasks tasks={tasks}/>
     </div>
   )
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,27 +1,4 @@
-import {useState} from 'react'
-
-const Tasks = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Buy Milk',
-            day: 'Feb 5th at 2:30pm',
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: 'Walk the dog',
-            day: 'Feb 6th at 6:20pm',
-            reminder: true,
-        },
-        {
-            id: 3,
-            text: 'Meeting',
-            day: 'Feb 7th at 13:00pm',
-            reminder: true,
-        }
-    ])
-
+const Tasks = ({tasks}) => {
     return(
         <>
             {tasks.map((task) => (
