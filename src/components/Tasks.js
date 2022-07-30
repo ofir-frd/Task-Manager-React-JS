9b27375564d03d6bcdd7b1bb49f94000500b4694
ofir-frd <ofir.friedman@materials.zone>
2022-07-30 15:10:10 +0300

    setTasks useState

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,33 @@
-const tasks = []
+import {useState} from 'react'
 
 const Tasks = () => {
-    return(
-        <div>
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Buy Milk',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: 'Walk the dog',
+            day: 'Feb 6th at 6:20pm',
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: 'Meeting',
+            day: 'Feb 7th at 13:00pm',
+            reminder: true,
+        }
+    ])
 
-        </div>
+    return(
+        <>
+            {tasks.map((task) => (
+                <h3 key={task.id}>{task.text}</h3>
+            ))}
+        </>
     )
 }
 
