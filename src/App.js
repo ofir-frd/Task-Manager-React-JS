074ee8ca379b9aa9649c70ef7c5f1a4120526502
ofir-frd <ofir.friedman@materials.zone>
2022-07-30 18:20:27 +0300

    Add Task conponent and delete button

--- src/App.js
+++ src/App.js
@@ -25,10 +25,15 @@ function App() {
         }
     ])
 
+    // Delete Task
+    const deleteTask = (id) => {
+       setTasks(tasks.filter((task) => task.id !== id))
+    }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   )
 }
