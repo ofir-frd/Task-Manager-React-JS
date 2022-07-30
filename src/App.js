f33cd0c55b44c8fe617c931e6723cc9d2ea48596
ofir-frd <ofir.friedman@materials.zone>
2022-07-30 10:54:32 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+    const name = "my name"
+  return (
+    <div className="App">
+      <h1> Hello world from my first react App</h1>
+        <h2>Hello2 {name}</h2>
+    </div>
+
+  );
+}
+
+export default App;
