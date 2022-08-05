a3a089d31abc6d892cbbc0cc74f690951d33faaf
ofir-frd <ofir.friedman@materials.zone>
2022-08-05 16:09:50 +0300

    json DB

--- src/App.js
+++ src/App.js
@@ -7,26 +7,7 @@ import AddTask from './components/AddTask'
 function App() {
     const [showAddTask, setShowAddTask] = useState(false)
 
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Buy Milk',
-            day: 'Feb 5th at 2:30pm',
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: 'Walk the dog',
-            day: 'Feb 6th at 6:20pm',
-            reminder: true,
-        },
-        {
-            id: 3,
-            text: 'Meeting',
-            day: 'Feb 7th at 13:00pm',
-            reminder: true,
-        }
-    ])
+    const [tasks, setTasks] = useState([])
 
     // Add Task
     const addTask = (task) => {
